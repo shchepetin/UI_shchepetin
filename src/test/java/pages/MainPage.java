9a44e767a0f3e81440063fe37d51705801d152c8
Щепетин Илья <shchepetin.i@dornet.ru>
2025-08-08 14:08:52 +0300

    Rollback MainPage

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,29 +1,21 @@
+
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Condition.attribute;
+
 
 public class MainPage {
 
-    private final SelenideElement
-            academyLink = $("[data-testid='header-navMenu'] a[href='/academy/about']"),
-            teamLink = $(byXpath("//nav[@data-testid='header-navMenu']//a[contains(text(), 'команды')]")),
-            storeLink = $("[data-testid='header-navMenu'] a[href*='store.spartak.com']"),
-            authLink = $("[data-testid='header-userinfo-authorization']"),
-            languageSwitcher = $("[data-testid='header-change-localization']");
+    private final SelenideElement academyLink = $("[data-testid='header-navMenu'] a[href='/academy/about']");
+    private final SelenideElement teamLink = $(byXpath("//nav[@data-testid='header-navMenu']//a[contains(text(), 'команды')]"));
+    private final SelenideElement storeLink = $("[data-testid='header-navMenu'] a[href*='store.spartak.com']");
+    private final SelenideElement authLink = $("[data-testid='header-userinfo-authorization']");
 
     public MainPage openMainPage() {
         open("/");
-
-        if ($("html").getAttribute("lang").equals("en")) {
-            languageSwitcher.click();
-        }
-
-        $("html").shouldHave(attribute("lang", "ru"));
-
         return this;
     }
 
